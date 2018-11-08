e909ea12912ea5679d5d8d2344e20490ac254712
tristanneumann1 <tristanneumann1@gmail.com>
2018-11-07 16:36:10 -0800

    Complete remaining lessons;

--- lessons/13-server-rendering/server.js
+++ lessons/13-server-rendering/server.js
@@ -2,17 +2,32 @@ var express = require('express')
 var path = require('path')
 var compression = require('compression')
 
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { match, RouterContext } from 'react-router'
+import routes from './modules/routes'
+
 var app = express()
 
-app.use(compression())
-
-// serve our static stuff like index.css
-app.use(express.static(path.join(__dirname, 'public')))
-
-// send all requests to index.html so browserHistory works
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+app.get('*', (req, res) => {
+  match({ routes: routes, location: req.url }, (err, redirect, props) => {
+    const appHtml = renderToString(<RouterContext {...props}/>)
+    res.send(renderPage(appHtml))
+  })
 })
+
+function renderPage(appHtml) {
+  return `
+    <!doctype html public="storage">
+    <html>
+    <meta charset=utf-8/>
+    <title>My First React Server Router App</title>
+    <link rel=stylesheet href=/index.css>
+    <div> hi from the server </div>
+    <div id=app>${appHtml}</div>
+    <script src="/bundle.js"></script>
+   `
+}
 
 var PORT = process.env.PORT || 8080
 app.listen(PORT, function() {
